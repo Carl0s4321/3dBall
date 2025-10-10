1a7c114304dad7d3d2d2c6fc2fdae2f17a3178bf
Carlos Sujanto <carlos.sujanto@ucalgary.ca>
2025-10-10 13:38:41 -0600

    +ball

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { useRef } from 'react'
+import * as THREE from 'three'
 
-const App = () => {
+export default function App() {
   return (
-    <div>App</div>
+    <Canvas
+      camera={{ position: [0, 0, 4], fov: 45 }}
+      style={{ width: '100vw', height: '100vh', background: '#111' }}
+    >
+      <directionalLight position={[1, 1, 2]} intensity={1.2} />
+      <ambientLight intensity={0.3} />
+      <Ball/>
+      <OrbitControls autoRotate />
+    </Canvas>
   )
 }
 
-export default App
+function Ball() {
+  const ref = useRef()
+
+  return (
+    <mesh ref={ref}>
+      <sphereGeometry args={[1, 64, 64]} />
+      <meshStandardMaterial color="#66aaff" roughness={0.4} metalness={0.1} />
+    </mesh>
+  )
+}
